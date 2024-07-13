3cdba55ac46b6f6d62683b34d5f9759d79c0f3b5
Sayan Paul <131338948+Sp-Sayan@users.noreply.github.com>
2024-07-13 20:46:16 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,7 +27,7 @@ const Login = (props) => {
       try {
         setEmptyData(false);
         setDisplay(false);
-        const response = await fetch("http://localhost:3001/login", {
+        const response = await fetch("https://authentication-app-backend-five.vercel.app/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
